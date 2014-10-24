6634e9dbe4bd9ecd9dcdf2d084da1be69c3e3e64
ramanad <ramana.dodla@gmail.com>
2014-10-23 23:55:36 -0500

    Finding inverse of a matrix using cached computation
    There are two functions. The first one `makeCacheMatrix' returns a list
    of
    -- set(x)  (setting the matrix `x')
    -- get() (get the matrix back)
    -- compute_inv() (function to compute the inverse of `x')
    -- getinv() (function to return the inverse)
    
    The second function cacheSolve(x, ...) computes the inverse if it is not
    in cache, or returns the inverse if it is in the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,54 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  #### the matrix inverse is set to null, when 'x' is assigned for the first time.
+  inv <- NULL
+  
+  #### function resets the matrix 'x', and then its inverse is set to NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  #### calculate inverse, and then update 'inv'
+  compute_inv <- function(...){
+    inv <<- solve(x, ...)
+  }
+  
+  #### get the matrix:
+  get <- function() {
+    x
+  }
+  
+  ### get matrix inverse:
+  getinv <- function(){
+    inv
+  }
 
+  list(set = set, get = get, compute_inv = compute_inv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Write a short comment describing this functi
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    
+    #### simply get the inverse from the makeCacheMatrix object `x'
+    inv <- x$getinv()
+    
+    #### return the cached inverse, if exists
+    if(!is.null(inv)){
+      message("getting cached inverse")
+      return(inv)
+    }
+    
+    ### If not, find the inverse
+    my_dat <- x$get()
+    inv <- x$compute_inv(...)
+    
+    inv
 }
+
+
